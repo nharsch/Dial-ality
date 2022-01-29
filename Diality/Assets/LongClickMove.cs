8c49798216794683c420a2b2ee5009b1a2c1e42a
Tanner Eckmann <talecbiz@yahoo.com>
2022-01-29 15:14:30 -0800

    Mountians prefab and sun

---
+++ Diality/Assets/LongClickMove.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongClickMove : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	public float direction = -1f;
+	public float moveSpeed = 50f;
+	public bool pointerDown = false;
+	public GameObject movableObj;
+	public float maxBound = 60f;
+	private float change = 0f;
+	private float bound = 0f;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		pointerDown = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		pointerDown = false;
+	}
+
+	private void Update()
+	{
+		if (pointerDown)
+		{
+			change = 1 * moveSpeed * direction * Time.deltaTime;
+			bound += change;
+			movableObj.transform.Translate(Vector3.up * change);
+
+			if ((bound > maxBound) || (bound < -maxBound))
+			{
+				direction = direction * -1;
+				//movableObj.transform.Translate(Vector3.up * change);
+			}
+		}
+		
+	}
+
+
+
+}
